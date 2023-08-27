86ee09da1c70a91bf1e40a125162a6a184f761e5
Nayan Jyoti Nath <121446398+includeNayan@users.noreply.github.com>
2023-08-27 19:36:52 +0530

    3

--- 3.c
+++ 3.c
@@ -1,102 +1,65 @@
 //NAYAN JYOTI NATH
 //2212068
-//to multiply two very large numbers
-//TC O(len1 * len2) SC O(len1+len2)
+//to divide two numbers
+//TC O(n^2!) SC O(n)
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-// Multiplies str1 and str2, and prints result.
-void multiply(char num1[], char num2[], char result[])
+// A function to perform division of large numbers
+char* longDivision(const char* number, int divisor)
 {
-    int len1 = strlen(num1);
-    int len2 = strlen(num2);
-    if (len1 == 0 || len2 == 0)
-    {
-        result[0] = '0';
-        result[1] = '\0';
-        return;
-    }
+    // As result can be very large store it in string
+    char* ans = (char*)malloc((strlen(number) + 1) * sizeof(char));
+    int ansIdx = 0;
 
-    // will keep the result number as an integer array
-    // in reverse order
-    int maxLen = len1 + len2;
-    int resultArray[maxLen];
-    for (int i = 0; i < maxLen; i++)
-    {
-        resultArray[i] = 0;
-    }
-
-    int i_n1 = 0;
-    int i_n2 = 0;
-
-    // Go from right to left in num1
-    for (int i = len1 - 1; i >= 0; i--)
-    {
-        int carry = 0;
-        int n1 = num1[i] - '0';
-        i_n2 = 0;
-
-        // Go from right to left in num2
-        for (int j = len2 - 1; j >= 0; j--)
-        {
-            int n2 = num2[j] - '0';
-            int sum = n1 * n2 + resultArray[i_n1 + i_n2] + carry;
-            carry = sum / 10;
-            resultArray[i_n1 + i_n2] = sum % 10;
-            i_n2++;
-        }
-
-        if (carry > 0)
-            resultArray[i_n1 + i_n2] += carry;
-
-        i_n1++;
-    }
-
-    int i = maxLen - 1;
-    while (i >= 0 && resultArray[i] == 0)
-        i--;
-
-    // If all were '0's - means either both or
-    // one of num1 or num2 were '0'
-    if (i == -1)
-    {
-        result[0] = '0';
-        result[1] = '\0';
-        return;
-    }
-
-    // Generate the result string
+    // Find prefix of number that is larger
+    // than divisor.
     int idx = 0;
-    while (i >= 0)
-    {
-        result[idx++] = resultArray[i] + '0';
-        i--;
+    int temp = number[idx] - '0';
+    while (temp < divisor)
+        temp = temp * 10 + (number[++idx] - '0');
+
+    // Repeatedly divide divisor with temp. After
+    // every division, update temp to include one
+    // more digit.
+    while (number[idx] != '\0') {
+        // Store result in answer i.e. temp / divisor
+        ans[ansIdx++] = (temp / divisor) + '0';
+
+        // Take next digit of number
+        temp = (temp % divisor) * 10 + number[++idx] - '0';
     }
-    result[idx] = '\0';
-    return;
+
+    // Null-terminate the result string
+    ans[ansIdx] = '\0';
+
+    // If divisor is greater than number
+    if (ans[0] == '\0') {
+        char* result = (char*)malloc(2 * sizeof(char));
+        result[0] = '0';
+        result[1] = '\0';
+        free(ans);
+        return result;
+    }
+
+    // else return ans
+    return ans;
 }
 
 
 int main()
 {
-    char str1[1000];
-    char str2[1000];
-    char result[1000];
-    printf("enter first number: ");
-    scanf("%s",str1);
-    printf("enter second number: ");
-    scanf("%s",str2);
-    if ((str1[0] == '-' || str2[0] == '-') &&
-        (str1[0] != '-' || str2[0] != '-'))
-        printf("-");
-
-    if (str1[0] == '-')
-        memmove(str1, str1 + 1, strlen(str1));
-
-    if (str2[0] == '-')
-        memmove(str2, str2 + 1, strlen(str2));
-
-    multiply(str1, str2, result);
+    char* number;
+    char num[1000];
+    int divisor;
+    printf("enter number: ");
+    scanf("%s",num);
+    number=num;
+    printf("enter divisor: ");
+    scanf("%d",&divisor);
+    char* result = longDivision(number, divisor);
     printf("%s\n", result);
+    free(result);
     return 0;
 }
