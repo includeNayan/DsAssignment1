e8105b5650df34103d2e71c2d5456ed35020bed1
Nayan Jyoti Nath <121446398+includeNayan@users.noreply.github.com>
2023-08-27 19:41:57 +0530

    4

--- 4.c
+++ 4.c
@@ -1,102 +1,52 @@
 //NAYAN JYOTI NATH
 //2212068
-//to multiply two very large numbers
-//TC O(len1 * len2) SC O(len1+len2)
+//TO FIND DIVISION UPTO NTH DECIMAL PLACE
+//WITHOUT GMP LIBRARY
+//TC O(n) SC O(1)
 #include <stdio.h>
-#include <string.h>
 
-// Multiplies str1 and str2, and prints result.
-void multiply(char num1[], char num2[], char result[])
-{
-    int len1 = strlen(num1);
-    int len2 = strlen(num2);
-    if (len1 == 0 || len2 == 0)
-    {
-        result[0] = '0';
-        result[1] = '\0';
+void Compute(int x, int y, int n) {
+    // Base cases
+    if (y == 0) {
+        printf("Infinite\n");
+        return;
+    }
+    if (x == 0) {
+        printf("0\n");
+        return;
+    }
+    if (n <= 0) {
+        // Since n <= 0, no more computation after the decimal
+        printf("%d\n", x / y);
         return;
     }
 
-    // will keep the result number as an integer array
-    // in reverse order
-    int maxLen = len1 + len2;
-    int resultArray[maxLen];
-    for (int i = 0; i < maxLen; i++)
-    {
-        resultArray[i] = 0;
+    // for negative numbers
+    if (((x > 0) && (y < 0)) || ((x < 0) && (y > 0))) {
+        printf("-");
+        x = x > 0 ? x : -x;
+        y = y > 0 ? y : -y;
     }
 
-    int i_n1 = 0;
-    int i_n2 = 0;
+    //division
+    int d = x / y;
 
-    // Go from right to left in num1
-    for (int i = len1 - 1; i >= 0; i--)
-    {
-        int carry = 0;
-        int n1 = num1[i] - '0';
-        i_n2 = 0;
-
-        // Go from right to left in num2
-        for (int j = len2 - 1; j >= 0; j--)
-        {
-            int n2 = num2[j] - '0';
-            int sum = n1 * n2 + resultArray[i_n1 + i_n2] + carry;
-            carry = sum / 10;
-            resultArray[i_n1 + i_n2] = sum % 10;
-            i_n2++;
-        }
-
-        if (carry > 0)
-            resultArray[i_n1 + i_n2] += carry;
-
-        i_n1++;
+    // Now one by print digits after dot using simple division.
+    for (int i = 0; i <= n; i++) {
+        printf("%d", d);
+        x = x - (y * d);
+        if (x == 0)
+            break;
+        x = x * 10;
+        d = x / y;
+        if (i == 0)
+            printf(".");
     }
-
-    int i = maxLen - 1;
-    while (i >= 0 && resultArray[i] == 0)
-        i--;
-
-    // If all were '0's - means either both or
-    // one of num1 or num2 were '0'
-    if (i == -1)
-    {
-        result[0] = '0';
-        result[1] = '\0';
-        return;
-    }
-
-    // Generate the result string
-    int idx = 0;
-    while (i >= 0)
-    {
-        result[idx++] = resultArray[i] + '0';
-        i--;
-    }
-    result[idx] = '\0';
-    return;
+    printf("\n");
 }
 
-
-int main()
-{
-    char str1[1000];
-    char str2[1000];
-    char result[1000];
-    printf("enter first number: ");
-    scanf("%s",str1);
-    printf("enter second number: ");
-    scanf("%s",str2);
-    if ((str1[0] == '-' || str2[0] == '-') &&
-        (str1[0] != '-' || str2[0] != '-'))
-        printf("-");
-
-    if (str1[0] == '-')
-        memmove(str1, str1 + 1, strlen(str1));
-
-    if (str2[0] == '-')
-        memmove(str2, str2 + 1, strlen(str2));
-
-    multiply(str1, str2, result);
-    printf("%s\n", result);
+int main() {
+    int x = 22, y = 7, n = 19;
+    Compute(x, y, n);
     return 0;
 }
